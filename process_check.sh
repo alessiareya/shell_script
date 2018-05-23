14f1b3e3ab6aadd953d37f9ca3424a30e63804d9
hatanoyoshihiko <hatanoyoshihiko@gmail.com>
2018-05-23 22:19:37 +0900

    bug fix
    httpd -tの出力が捨てられないので要修正。一応、動く。

--- process_check.sh
+++ process_check.sh
@@ -3,24 +3,27 @@
 SERVICE="httpd.service"
 
 while true; do
-  HTTPD_CHECK=`ps aux | grep httpd | grep -v grep | wc -l`
+  HTTPD_CHECK=`ps aux | grep httpd | grep -v grep -c`
 
   if [ ${HTTPD_CHECK} = 0 ]; then
-    systemctl status httpd.service > /dev/null 2>&1
+    systemctl start httpd.service && echo "${SERVICE} is started"
     HTTPD_STATUS=$?
+
     case ${HTTPD_STATUS} in
       "0")
-        echo "${SERVICE} is alived."
+        :
         ;;
-      *)
-        /usr/sbin/httpd -t 2>&1 | grep -i "syntax error"
+      "1")
+        { /sbin/httpd -t 2>&1 | grep -i "syntax error"; } > /dev/null 2>&1
         CONFIG_STATUS=$?
+
         if [ ${CONFIG_STATUS} = 0 ]; then
           echo "Please Check httpd.conf"
           exit 0
         elif [ ${CONFIG_STATUS} = 1 ]; then
           systemctl start httpd.service > /dev/null 2>&1
-          echo "${SERVICE} is started."
+          echo "${SERVICE} is alived."
+          sleep 5
         else
           echo "Please Check error_log."
           exit 0
