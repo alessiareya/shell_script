2fa0541c58cd338fcd3c0a229bdd15d2f13615a7
hatanoyoshihiko <hatanoyoshihiko@gmail.com>
2018-05-31 10:16:48 +0900

    add all startup.
    全台起動するか、1台ずつ起動するかに変更。

--- startup_vagrant.sh
+++ startup_vagrant.sh
@@ -1,32 +1,51 @@
 #!/bin/bash
 
-vagrant_dir=~/Vagrant/
+## environment variable
+VAGRANT_DIR=~/Vagrant/
 
-for VM in `ls ${vagrant_dir}`
-do
-  echo "Do you want to startup ${VM}？　Please input \"y\" or \"n\""
-  read yn
+## all VMs startup
+echo "Do you want to start all VMs? input \"y\" or \"n\""
+read yn
 
-  if [ $yn = y ]; then
-    cd ${vagrant_dir}${VM}/
+if [ ${yn} = y ]; then
+  for VM in `ls ${VAGRANT_DIR}`;
+  do
+    cd ${VAGRANT_DIR}${VM}/
     vagrant status | grep poweroff > /dev/null
-    VMSTATE=$?
-
-    case ${VMSTATE} in
+    VM_STATE=$?
+    case ${VM_STATE} in
       0)
-        vagrant up && echo "VM ${VM} is started."
-        ;;
+        vagrant up && echo "VM \"${i}\" is started." ;;
       1)
-        echo "VM ${VM} is running."
-        ;;
+        echo "VM \"${VM}\" is already running." ;;
     esac
+  done
+elif [ ${yn} = n ]; then
+  echo "select VM to be activated."
+  for VM in `ls ${VAGRANT_DIR}`;
+  do
+    echo "Do you want to startup VM \"${VM}\"? input \"y\" or \"n\""
+    read yn
+    if [ ${yn} = y ]; then
+      cd ${VAGRANT_DIR}${VM}/
+      vagrant status | grep poweroff > /dev/null
+      VMSTATE=$?
+      case ${VMSTATE} in
+        0)
+          vagrant up && echo "VM \"${VM}\" is started." ;;
+        1)
+          echo "VM \"${VM}\" is already running." ;;
+      esac
+    elif [ ${yn} = n ]; then
+      echo "Starting VM \"${VM}\" was canceled."
+    else
+      echo "input \"y\" or \"n\""
+    fi
+  done
+else
+    echo "input \"y\" or \"n\""
+fi
 
-  elif [ $yn = n ]; then
-    echo "Starting VM ${VM} was canceled."
-  else
-    echo "Please input \"y\" or \"n\""
-  fi
-done
-
+## Processing after startup
 echo "Display VM's state."
 vagrant global-status
