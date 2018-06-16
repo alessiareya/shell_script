be9f51a071ca1e82dfd060e61c7fd451c0258c83
hatanoyoshihiko <hatanoyoshihiko@gmail.com>
2018-06-16 21:56:15 +0900

    rename and bug fix
    ・ファイル名を変更
    ・ファイル数カウントとファイルサイズカウントを別々の処理にしていたのを1回に集約
    ・リモート側のファイルサイズ合計値計算に誤りがあるのを修正

--- rsync_and_check.sh
+++ rsync_and_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# ENVIRONMENT VARIABLE
+## ENVIRONMENT VARIABLE
 SRC_DIR="/data/"
 DEST_USER="root"
 DEST_HOST="192.168.10.151"
@@ -10,7 +10,7 @@ KEY=~/.ssh/id_rsa
 LOG_FILE="log_`date "+%Y%m%d_%H%M%S.log"`"
 MAIL_ADDRESS="root@localhost"
 
-# RSYNC PRE TEST
+## RSYNC PRE TEST
 ssh ${DEST_USER}@${DEST_HOST} -i ${KEY} 'exit' > /dev/null
 SSH_STATUS=$?
 
@@ -22,7 +22,7 @@ case ${SSH_STATUS} in
      exit 0 ;;
 esac
 
-# RUN RSYNC
+## RUN RSYNC
 case ${yn} in
   "y") echo "rsync will be started."
        rsync ${RSYNC_OPTION} --log-file=${LOG_FILE} ${SRC_DIR} ${DEST_USER}@${DEST_HOST}:${DEST_DIR} > /dev/null
@@ -41,28 +41,23 @@ else
   echo "rsync was successful."
 fi
 
-# after rsync count checksum, files, file size
+## after rsync count checksum, files, file size
 echo "Do you count the checksum, number of files, file size from destination directory? \"y\" or \"n\""
 read yn
 
 if [ ${yn} = y ]; then
   LOCAL_CHECK_SUM=`find ${DEST_DIR} -type f -exec md5sum {} \; | sort | md5sum`
-  LOCAL_COUNT_FILES=`find ${DEST_DIR} -type f | wc -l`
-  LOCAL_COUNT_FILE_SIZE=`find ${DEST_DIR} -type f -printf "%s\n" | awk 'BEGIN { sum = 0; } { sum += $1; } END { print sum; }'`
-
+  LOCAL_COUNT=`find ${DEST_DIR} -type f -printf "%p %s\n" | awk 'BEGIN { sum = 0; } { sum += $2; } END { print "file count total is " NR, "file size total is " sum; }'`
   REMOTE_CHECK_SUM=`ssh ${DEST_USER}@${DEST_HOST} -i ${KEY} "find ${DEST_DIR} -type f -exec md5sum {} \; | sort | md5sum"`
-  REMOTE_COUNT_FILES=`ssh ${DEST_USER}@${DEST_HOST} -i ${KEY} "find ${DEST_DIR} -type f | wc -l"`
-  REMOTE_COUNT_FILE_SIZE=`ssh ${DEST_USER}@${DEST_HOST} -i ${KEY} "find ${DEST_DIR} -type f -printf \%s\n\"" | awk 'BEGIN { sum = 0; } { sum += $1; } END { print sum; }'`
+  REMOTE_COUNT=`ssh ${DEST_USER}@${DEST_HOST} -i ${KEY} "find ${DEST_DIR} -type f -printf \"%p %s\n\"" | awk 'BEGIN { sum = 0; } { sum += $2; } END { print "file count total is " NR, "file size total is " sum; }'`
 
   echo "########## LOCAL HOST RESURT ##########"
   echo "checksum is \"${LOCAL_CHECK_SUM}\""
-  echo "total files is \"${LOCAL_COUNT_FILES}\""
-  echo "file size total is \"${LOCAL_COUNT_FILE_SIZE}\"" && echo ""
+  echo ${LOCAL_COUNT} && echo ""
 
   echo "########## REMOTE HOST RESULT ##########"
   echo "checksum is \"${REMOTE_CHECK_SUM}\""
-  echo "total files is \"${REMOTE_COUNT_FILES}\""
-  echo "file size total is \"${REMOTE_COUNT_FILE_SIZE}\""
+  echo ${REMOTE_COUNT}
 
 elif [ ${yn} = n ]; then
   echo "Processing ends."
