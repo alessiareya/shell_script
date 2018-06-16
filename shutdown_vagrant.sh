1b6bc5e5951f956e3413b8ff5deb3803659610e9
hatanoyoshihiko <hatanoyoshihiko@gmail.com>
2018-06-16 09:01:19 +0900

    update
    readの処理を-pに変更
    echoの内容を修正

--- shutdown_vagrant.sh
+++ shutdown_vagrant.sh
@@ -4,8 +4,7 @@
 VAGRANT_DIR=~/Vagrant/
 
 ## all VMs shutdown
-echo "Do you want to shutdown all VMs? input \"y\" or \"n\""
-read yn
+read -p "Do you want to shutdown all VMs? input \"y\" or \"n\"" yn
 
 if [ ${yn} = y ]; then
   for VM in `ls ${VAGRANT_DIR}`;
@@ -15,29 +14,28 @@ if [ ${yn} = y ]; then
     VM_STATE=$?
     case ${VM_STATE} in
       0)
-        vagrant halt > /dev/null && echo "VM \"${VM}\" is shutdowned." ;;
+        vagrant halt > /dev/null && echo "##### VM \"${VM}\" is shutdowned. #####" ;;
       1)
-        echo "VM \"${VM}\" is already shutdowned." ;;
+        echo "##### VM \"${VM}\" is already shutdowned. #####" ;;
     esac
   done
 elif [ ${yn} = n ]; then
   echo "select VM to be shutdowned."
   for VM in `ls ${VAGRANT_DIR}`;
   do
-    echo "Do you want to shutdown VM \"${VM}\"? input \"y\" or \"n\""
-    read yn
+    read -p "Do you want to shutdown VM \"${VM}\"? input \"y\" or \"n\"" yn
     if [ ${yn} = y ]; then
       cd ${VAGRANT_DIR}${VM}/
       vagrant status | grep running > /dev/null
       VMSTATE=$?
       case ${VMSTATE} in
         0)
-          vagrant halt > /dev/null && echo "VM \"${VM}\" is shutdowned." ;;
+          vagrant halt > /dev/null && echo "##### VM \"${VM}\" is shutdowned. #####" ;;
         1)
-          echo "VM \"${VM}\" is already shutdowned." ;;
+          echo "##### VM \"${VM}\" is already shutdowned. #####" ;;
       esac
     elif [ ${yn} = n ]; then
-      echo "Shutdown VM \"${VM}\" was canceled."
+      echo "##### Shutdown VM \"${VM}\" was canceled. #####"
     else
       echo "input \"y\" or \"n\""
     fi
@@ -47,5 +45,5 @@ else
 fi
 
 ## Processing after startup
-echo "Display VM's state."
+echo "##### Display VM's state. #####"
 vagrant global-status
--- startup_vagrant.sh
+++ startup_vagrant.sh
@@ -4,8 +4,7 @@
 VAGRANT_DIR=~/Vagrant/
 
 ## all VMs startup
-echo "Do you want to start all VMs? input \"y\" or \"n\""
-read yn
+read -p "Do you want to start all VMs? input \"y\" or \"n\"" yn
 
 if [ ${yn} = y ]; then
   for VM in `ls ${VAGRANT_DIR}`;
@@ -15,29 +14,28 @@ if [ ${yn} = y ]; then
     VM_STATE=$?
     case ${VM_STATE} in
       0)
-        vagrant up > /dev/null && echo "VM \"${VM}\" is started." ;;
+        vagrant up > /dev/null && echo "##### VM \"${VM}\" is started. #####" ;;
       1)
-        echo "VM \"${VM}\" is already running." ;;
+        echo "##### VM \"${VM}\" is already running. #####" ;;
     esac
   done
 elif [ ${yn} = n ]; then
   echo "select VM to be activated."
   for VM in `ls ${VAGRANT_DIR}`;
   do
-    echo "Do you want to startup VM \"${VM}\"? input \"y\" or \"n\""
-    read yn
+    read -p "Do you want to startup VM \"${VM}\"? input \"y\" or \"n\"" yn
     if [ ${yn} = y ]; then
       cd ${VAGRANT_DIR}${VM}/
       vagrant status | grep poweroff > /dev/null
       VMSTATE=$?
       case ${VMSTATE} in
         0)
-          vagrant up > /dev/null && echo "VM \"${VM}\" is started." ;;
+          vagrant up > /dev/null && echo "VM \"##### ${VM}\" is started. #####" ;;
         1)
-          echo "VM \"${VM}\" is already running." ;;
+          echo "##### VM \"${VM}\" is already running. #####" ;;
       esac
     elif [ ${yn} = n ]; then
-      echo "Starting VM \"${VM}\" was canceled."
+      echo "##### Starting VM \"${VM}\" was canceled. #####"
     else
       echo "input \"y\" or \"n\""
     fi
@@ -47,5 +45,5 @@ else
 fi
 
 ## Processing after startup
-echo "Display VM's state."
+echo "##### Display VM's state. #####"
 vagrant global-status
