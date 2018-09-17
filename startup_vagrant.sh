1953f674849c26916e7d29833df1e5f6a3e27d87
hatanoyoshihiko <hatanoyoshihiko@gmail.com>
2018-09-17 21:43:52 +0900

    VMの起動処理を選択式に変更。
    順番に起動させるときにforではなくselectで選択出来る処理に更新。

--- startup_vagrant.sh
+++ startup_vagrant.sh
@@ -2,32 +2,30 @@
 
 ## environment variable
 VAGRANT_DIR=~/Vagrant/
+VM_NAME=`ls ${VAGRANT_DIR}`
+VM_DIR=/Users/alessi/Vagrant
 
-## all VMs startup
-read -p "Do you want to start all VMs? input \"y\" or \"n\"" yn
+## Functions
+ALL_VM_STARTUP()
+{
+  cd ${VAGRANT_DIR}${VM}
+  VM_STATUS=`vagrant status | grep default | awk '{ print $2 }'`
+  if [ ${VM_STATUS} = "poweroff" ]; then
+    vagrant up > /dev/null && echo "##### VM \"${VM}\" is started. #####" 
+  elif [ ${VM_STATUS} = "running" ]; then
+    echo "##### VM \"${VM}\" is already running. #####"
+  elif [ ${VM_STATUS} = "saved" ]; then
+    vagrant resume > /dev/null && echo "##### VM \"${VM}\" resumed from suspend. #####"
+  else
+    vagrant up > /dev/null && echo "##### VM \"${VM}\" is started from aborted. #####" 
+  fi
+} 
 
-if [ ${yn} = y ]; then
-  for VM in `ls ${VAGRANT_DIR}`;
-  do
-    cd ${VAGRANT_DIR}${VM}/
-    VM_STATUS=`vagrant status | grep default | awk '{ print $2 }'`
-    if [ ${VM_STATUS} = "poweroff" ]; then
-      vagrant up > /dev/null && echo "##### VM \"${VM}\" is started. #####" 
-    elif [ ${VM_STATUS} = "running" ]; then
-      echo "##### VM \"${VM}\" is already running. #####"
-    elif [ ${VM_STATUS} = "saved" ]; then
-      vagrant resume > /dev/null && echo "##### VM \"${VM}\" resumed from suspend. #####"
-    else
-      vagrant up > /dev/null && echo "##### VM \"${VM}\" is started from aborted. #####" 
-    fi
-  done
-elif [ ${yn} = n ]; then
-  echo "select VM to be activated."
-  for VM in `ls ${VAGRANT_DIR}`;
-  do
-    read -p "Do you want to startup VM \"${VM}\"? input \"y\" or \"n\"" yn
-    if [ ${yn} = y ]; then
-      cd ${VAGRANT_DIR}${VM}/
+SELECT_VM_STARTUP()
+{
+  case $VM in
+    "${VM%quit}") 
+	    cd ${VM_DIR}/${VM}
       VM_STATUS=`vagrant status | grep default | awk '{ print $2 }'`
       if [ ${VM_STATUS} = "poweroff" ]; then
         vagrant up > /dev/null && echo "##### VM \"${VM}\" is started. #####" 
@@ -38,15 +36,36 @@ elif [ ${yn} = n ]; then
       else
         echo "##### UNKNOWN STATUS #####"
       fi
-    elif [ ${yn} = n ]; then
-      echo "##### Starting VM \"${VM}\" was canceled. #####"
-    else
-      echo "input \"y\" or \"n\""
-    fi
+			;;
+    "quit")
+      echo "Processing ends"
+      break
+			;;
+    *)
+      echo ${REPLY} is not exist.
+  esac
+}
+
+## VMs startup process
+read -p "Do you want to start all VMs? input \"y\" or \"n\"" yn
+
+### All VMs startup
+if [ ${yn} = y ]; then
+  for VM in ${VM_NAME};
+  do
+  ALL_VM_STARTUP
   done
+
+### select VMs startup.
+elif [ ${yn} = n ]; then
+  echo "Select VM."
+  select VM in ${VM_NAME} quit
+  do
+  SELECT_VM_STARTUP
+  done	
 else
-    echo "input \"y\" or \"n\""
-fi
+  echo "input \"y\" or \"n\"" && exit 0
+fi	
 
 ## Processing after startup
 echo "##### Display VM's state. #####"
